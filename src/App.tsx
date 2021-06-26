8bc20d547209eedc7addaf23c27d3e561f19d9a0
kai <coelacanth2505@gmail.com>
2021-06-26 20:51:23 +0900

    hello

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.tsx</code> and save to reload. */}
-          hello,world!  hello,React!!by  coelacanth
+          hello,React!!byシーラカンス
         </p>
         <a
           className="App-link"
