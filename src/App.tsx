e7259a1a238c8d4c236bb8ac92b00eb61ba15483
kai <coelacanth2505@gmail.com>
2021-06-28 19:31:42 +0900

    522

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import "./styles.css";
 import './App.css';
 
-function App() {
+export const App = () => {
+  const [incompleteTodos, setIncompleteTodos] = useState(['羽が無い生き物が飛べたのは、羽が無かったから', 'いっそ飛べない鳥の羽なんかもがれちまえばいい']);
+  const [completeTodos,setCompleteTodos] = useState(['破り損なった手作りの地図'])
   return (
     // <div className="App">
       // <header className="App-header">
@@ -30,25 +32,28 @@ function App() {
     <div className="incomplete-area">
         <p className="title">未完</p>
         <ul>
-          <div className="list-row">
-            <li>羽が無い生き物が飛べたのは、羽が無かったから</li>
-            <button>Mission Complete</button>
-            <button>Delete</button>
-          </div>
-          <div className="list-row">
-            <li>いっそ飛べない鳥の羽なんかもがれちまえばいい</li>
-            <button>Mission Complete</button>
-            <button>Delete</button>
-          </div>
+          {incompleteTodos.map((todo) => {
+            return (
+              <div key={todo} className="list-row">
+                <li>{todo}</li>
+                <button>Mission Complete</button>
+                <button>Delete</button>
+              </div>
+            );
+          })}
         </ul>
     </div>
     <div className="complete-area">
       <p className="title">end</p>
         <ul>
-          <div className="list-row">
-            <li>破り損なった手作りの地図</li>
+          {completeTodos.map((todo) => {
+            return (
+          <div key={todo} className="list-row">
+            <li>{todo}</li>
             <button>return</button>
           </div>
+            )
+          })}
         </ul>
     </div>
     </>
