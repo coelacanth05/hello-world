b113c3f7bb639f182b2cff96aa34b027a65add49
kai <coelacanth2505@gmail.com>
2021-06-29 23:04:39 +0900

    53

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,16 @@ import "./styles.css";
 import './App.css';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState(['羽が無い生き物が飛べたのは、羽が無かったから', 'いっそ飛べない鳥の羽なんかもがれちまえばいい']);
-  const [completeTodos,setCompleteTodos] = useState(['破り損なった手作りの地図'])
+  const [completeTodos, setCompleteTodos] = useState(['破り損なった手作りの地図']);
+  const onChangeTodoText = (event: any) => setTodoText(event.target.value);
+  const onClickAdd = () => {
+    if (todoText === "") return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText("");
+  }
   return (
     // <div className="App">
       // <header className="App-header">
@@ -26,8 +34,8 @@ export const App = () => {
     // </div>
     <>
     <div className="input-area">
-        <input placeholder="input todo"/>
-        <button>Add</button>
+        <input placeholder="input todo" value={todoText} onChange={onChangeTodoText}/>
+        <button onClick={onClickAdd}>Add</button>
     </div>
     <div className="incomplete-area">
         <p className="title">未完</p>
